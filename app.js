23c3fa034ba4135c9986a2b5436147067b534cdc
lavimalik <usefulshine@gmail.com>
2018-12-18 23:39:28 +0530

    Update app.js

--- app.js
+++ app.js
@@ -261,6 +261,29 @@ function pick(pos, state, dispatch) {
   dispatch({color: state.picture.pixel(pos.x, pos.y)})
 }
 
+function circle(pos, state, dispatch) {
+  function drawCircle(to) {
+    let radius = Math.sqrt(Math.pow(to.x - pos.x, 2) +
+                          Math.pow(to.y - pos.y, 2))
+    let radiusC = Math.ceil(radius)
+    let drawn = []
+    for (let dy = -radiusC; dy <= radiusC; dy++) {
+      for (let dx = -radiusC; dx <= radiusC; dx++) {
+        let dist = Math.sqrt(Math.pow(dx, 2) + Math.pow(dy, 2))
+        if (dist > radius) continue
+        let y = pos.y + dy, x = pos.x + dx
+        if (y < 0 || y >= state.picture.height ||
+          x < 0 || x >= state.picture.width) continue
+        drawn.push({x, y, color: state.color})
+      }
+    }
+    dispatch({picture: state.picture.draw(drawn)})
+  }
+  drawCircle(pos)
+  return drawCircle
+}
+
+
 class SaveButton {
   constructor(state) {
     this.picture = state.picture
@@ -393,28 +416,3 @@ function startPixelEditor({state = startState,
   })
   return app.dom
 }
-
-function circle(pos, state, dispatch) {
-  function drawCircle(to) {
-    let radius = Math.sqrt(Math.pow(to.x - pos.x, 2) +
-                          Math.pow(to.y - pos.y, 2))
-    let radiusC = Math.ceil(radius)
-    let drawn = []
-    for (let dy = -radiusC; dy <= radiusC; dy++) {
-      for (let dx = -radiusC; dx <= radiusC; dx++) {
-        let dist = Math.sqrt(Math.pow(dx, 2) + Math.pow(dy, 2))
-        if (dist > radius) continue
-        let y = pos.y + dy, x = pos.x + dx
-        if (y < 0 || y >= state.picture.height ||
-          x < 0 || x >= state.picture.width) continue
-        drawn.push({x, y, color: state.color})
-      }
-    }
-    dispatch({picture: state.picture.draw(drawn)})
-  }
-  drawCircle(pos)
-  return drawCircle
-}
-
-
-
